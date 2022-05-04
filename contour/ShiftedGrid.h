67c7ccb6ed8cbced1bbe9f35cd2648fef993d473
Mika Heiskanen <mika.heiskanen@fmi.fi>
2022-05-04 15:08:12 +0000

    Fixed ShiftedGrid not to try global wraparounds to avoid topology errors in contouring

--- contour/ShiftedGrid.h
+++ contour/ShiftedGrid.h
@@ -26,18 +26,19 @@ class ShiftedGrid : public Trax::Grid
         itsValidCells(theValidCells),
         itsMatrix(theMatrix),
         itsNX(theMatrix.NX()),
-        itsWidth(theCoords.width()),
         itsHeight(theCoords.height()),
         itsShift(theShift)
   {
-    // If the data is shifted and the data is world wrapped (one more coordinate than values),
-    // adjust back the width to avoid a duplicate column at the shift location
-    if (itsShift > 0 && itsWidth == itsNX + 1)
-      --itsWidth;
+    // Note that width is taken from data, not coordinates. Shifting usually
+    // effectively breaks contouring if we try to provide wraparound capabilities.
 
     if (theCoords.height() != theMatrix.NY() ||
         (theCoords.width() != theMatrix.NX() && theCoords.width() != theMatrix.NX() + 1))
       throw Fmi::Exception(BCP, "Contoured data and coordinate dimensions mismatch");
+
+    if (theShift == 0)
+      throw Fmi::Exception(
+          BCP, "A zero shift for ShiftedGrid disables global data wraparound, fix code");
   }
 
   // Provide wrap-around capability for world data
@@ -50,23 +51,28 @@ class ShiftedGrid : public Trax::Grid
 
   bool valid(long i, long j) const override
   {
-    if (i < 0 || j < 0 || static_cast<std::size_t>(i) >= itsWidth ||
+    if (i < 0 || j < 0 || static_cast<std::size_t>(i) >= itsNX ||
         static_cast<std::size_t>(j) >= itsHeight)
       return false;
     return itsValidCells((i + itsShift) % itsNX, j);
   }
 
-  std::size_t width() const override { return itsWidth; }
+  std::size_t width() const override { return itsNX; }
   std::size_t height() const override { return itsHeight; }
 
-  std::array<std::size_t, 4> bbox() const override { return itsValidCells.bbox(); }
+  std::array<std::size_t, 4> bbox() const override
+  {
+    auto ret = itsValidCells.bbox();  // xmin,ymin,xmax,ymax
+    // Prevent wraparound of cells. One minus for cells vs vertices, another for dropped cell
+    ret[2] = std::min(ret[2], itsNX - 2);
+    return ret;
+  }
 
  private:
   const Fmi::CoordinateMatrix& itsCoords;
   const Fmi::BoolMatrix& itsValidCells;
   NFmiDataMatrix<float>& itsMatrix;
   const std::size_t itsNX;  // coordinates width
-  std::size_t itsWidth;     // data width
   const std::size_t itsHeight;
   std::size_t itsShift = 0;  // horizontal shift for global data wraparound
 
