d69ed28f7005f36988ed7b2115410b1c186029f0
Mika Heiskanen <mika.heiskanen@fmi.fi>
2022-09-15 08:51:01 +0000

    Improvements to missing value contouring

--- contour/Grid.h
+++ contour/Grid.h
@@ -6,6 +6,7 @@
 #include <newbase/NFmiDataMatrix.h>
 #include <newbase/NFmiPoint.h>
 #include <trax/Grid.h>
+#include <limits>
 
 // Note: The values may be one column narrower than the coordinates if the data needs a wraparound
 // to fill the globe. In this case the last coordinate is the projected metric coordinate with
@@ -41,26 +42,73 @@ class Grid : public Trax::Grid
       throw Fmi::Exception(BCP, "Contoured data and coordinate dimensions mismatch");
   }
 
+  void shell(double maxcoord) { itsMaxCoord = maxcoord; }
+
+  long mini() const { return static_cast<long>(itsValidCells.bbox()[0]); }
+  long minj() const { return static_cast<long>(itsValidCells.bbox()[1]); }
+  long maxi() const { return static_cast<long>(itsValidCells.bbox()[2] + 2); }
+  long maxj() const { return static_cast<long>(itsValidCells.bbox()[3] + 2); }
+
   // Provide wrap-around capability for world data
 
-  double x(long i, long j) const override { return itsCoords.x(i, j); }
-  double y(long i, long j) const override { return itsCoords.y(i, j); }
-  double operator()(long i, long j) const override { return itsMatrix[i % itsNX][j]; }
+  double x(long i, long j) const override
+  {
+    double ret = 666.0;
+    if (i <= mini())
+      ret = -itsMaxCoord;
+    else if (i >= maxi() + 1)
+      ret = itsMaxCoord;
+    else
+      ret = itsCoords.x(i - 1, std::min(std::max(1L, j), maxj() - 1) - 1);
+    return ret;
+  }
+
+  double y(long i, long j) const override
+  {
+    double ret = 666.0;
+    if (j <= minj())
+      ret = -itsMaxCoord;
+    else if (j >= maxj() + 1)
+      ret = itsMaxCoord;
+    else
+      ret = itsCoords.y(std::min(std::max(1L, i), maxi() - 2), j - 1);
+    return ret;
+  }
+
+  double operator()(long i, long j) const override
+  {
+    double ret = 666.0;
+    if (i <= mini() || i >= maxi() + 1 || j <= minj() || j >= maxj() + 1)
+      ret = std::numeric_limits<double>::quiet_NaN();
+    else
+      ret = itsMatrix[(i - 1) % itsNX][j - 1];
+    return ret;
+  }
+
+  // When creating data we do not use shifts
   void set(long i, long j, double z) override { itsMatrix[i][j] = z; }
 
   bool valid(long i, long j) const override
   {
-    if (i < 0 || j < 0 || static_cast<std::size_t>(i) >= itsWidth ||
-        static_cast<std::size_t>(j) >= itsHeight)
-      return false;
-    return itsValidCells(i, j);
+    bool flag = false;
+
+    if (i <= mini() || i >= maxi() + 1 || j <= minj() || j >= maxj() + 1)
+      flag = true;  // but full of NaN
+    else
+      flag = itsValidCells(i - 1, j - 1);
+    return flag;
   }
 
   std::size_t width() const override { return itsWidth; }
   std::size_t height() const override { return itsHeight; }
   std::size_t shift() const override { return itsShift; }
 
-  std::array<std::size_t, 4> bbox() const override { return itsValidCells.bbox(); }
+  // We expand the grid by one cell in all directions to surround the data with missing values
+  std::array<std::size_t, 4> bbox() const override
+  {
+    const auto& box = itsValidCells.bbox();
+    return {box[0], box[1], box[2] + 2, box[3] + 2};
+  }
 
  private:
   const Fmi::CoordinateMatrix& itsCoords;
@@ -70,6 +118,8 @@ class Grid : public Trax::Grid
   const std::size_t itsWidth;  // data width
   const std::size_t itsHeight;
   const std::size_t itsShift;  // horizontal shift in start position
+
+  double itsMaxCoord = 1e10;
 
 };  // class Grid
 }  // namespace Contour
---
+++ test/GridTest.cpp
@@ -0,0 +1,155 @@
+#include "Grid.h"
+#include <gis/CoordinateMatrix.h>
+#include <gis/CoordinateMatrixAnalysis.h>
+#include <newbase/NFmiDataMatrix.h>
+#include <regression/tframe.h>
+#include <trax/Contour.h>
+#include <trax/IsobandLimits.h>
+#include <limits>
+
+using namespace std;
+
+namespace Tests
+{
+void normal()
+{
+  // Construct a simple grid with one corner value missing, and try contouring NaN...NaN
+  // to see if we get the expected missing area
+
+  NFmiDataMatrix<float> values(2, 2, kFloatMissing);
+  values[0][0] = 0;
+  values[0][1] = 2;
+  values[1][0] = 4;
+  values[1][1] = 6;
+
+  // 2x2 grid from 0,0 to 10,10
+  Fmi::CoordinateMatrix coords(2, 2, 0, 0, 10, 10);
+
+  // Analyze validity
+  auto analysis = std::make_shared<Fmi::CoordinateAnalysis>(Fmi::analysis(coords));
+
+  // Contouring API
+  SmartMet::Engine::Contour::Grid grid(values, coords, analysis->valid);
+  grid.shell(100);  // external shell for missing values
+
+  // Isoband for missing values
+  Trax::IsobandLimits limits;
+  limits.add(2, 4);
+
+  Trax::Contour contour;
+  contour.validate(true);
+  auto results = contour.isobands(grid, limits);
+
+  if (results.size() != 1)
+    TEST_FAILED("Failed to contour valid area");
+
+  string expected = "POLYGON ((0 10,5 10,10 0,5 0,0 10))";
+  auto wkt = results[0].wkt();
+  if (wkt != expected)
+    TEST_FAILED("Incorrect result: " + wkt + " <> " + expected);
+
+  TEST_PASSED();
+}
+
+void valid()
+{
+  // Construct a simple grid with one corner value missing, and try contouring NaN...NaN
+  // to see if we get the expected missing area
+
+  NFmiDataMatrix<float> values(2, 2, kFloatMissing);
+  values[0][0] = 0;
+  values[0][1] = 1;
+  values[1][0] = 2;
+  values[1][1] = 5;
+
+  // 2x2 grid from 0,0 to 1,1
+  Fmi::CoordinateMatrix coords(2, 2, 0, 0, 1, 1);
+
+  // Analyze validity
+  auto analysis = std::make_shared<Fmi::CoordinateAnalysis>(Fmi::analysis(coords));
+
+  // Contouring API
+  SmartMet::Engine::Contour::Grid grid(values, coords, analysis->valid);
+  grid.shell(10);  // external shell for missing values
+
+  // Isoband for missing values
+  Trax::IsobandLimits limits;
+  limits.add(-std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity());
+
+  Trax::Contour contour;
+  contour.validate(true);
+  auto results = contour.isobands(grid, limits);
+
+  if (results.size() != 1)
+    TEST_FAILED("Failed to contour valid area");
+
+  string expected = "POLYGON ((0 0,0 1,1 1,1 0,0 0))";
+  auto wkt = results[0].wkt();
+  if (wkt != expected)
+    TEST_FAILED("Incorrect result: " + wkt + " <> " + expected);
+
+  TEST_PASSED();
+}
+
+void missing()
+{
+  // Construct a simple grid with one corner value missing, and try contouring NaN...NaN
+  // to see if we get the expected missing area
+
+  NFmiDataMatrix<float> values(2, 2, kFloatMissing);
+  values[0][0] = std::numeric_limits<float>::quiet_NaN();
+  values[0][1] = 1;
+  values[1][0] = 2;
+  values[1][1] = 5;
+
+  // 2x2 grid from -1,-1 to 1,1
+  Fmi::CoordinateMatrix coords(2, 2, -1, -1, 1, 1);
+
+  // Analyze validity
+  auto analysis = std::make_shared<Fmi::CoordinateAnalysis>(Fmi::analysis(coords));
+
+  // Contouring API
+  SmartMet::Engine::Contour::Grid grid(values, coords, analysis->valid);
+  grid.shell(2);  // external shell for missing values
+
+  // Isoband for missing values
+  Trax::IsobandLimits limits;
+  limits.add(std::numeric_limits<double>::quiet_NaN(), std::numeric_limits<double>::quiet_NaN());
+
+  Trax::Contour contour;
+  contour.validate(true);
+  auto results = contour.isobands(grid, limits);
+
+  if (results.size() != 1)
+    TEST_FAILED("Failed to contour missing area");
+
+  string ok =
+      "POLYGON ((-2 -2,-2 -1,-2 1,-2 2,-1 2,1 2,2 2,2 1,2 -1,2 -2,1 -2,-1 -2,-2 -2),(1 -1,1 1,-1 "
+      "1,1 -1))";
+  auto wkt = results[0].wkt();
+  if (wkt != ok)
+    TEST_FAILED("Incorrect result: " + wkt + " <> " + ok);
+
+  TEST_PASSED();
+}
+
+class tests : public tframe::tests
+{
+  // Overridden message separator
+  virtual const char *error_message_prefix() const { return "\n\t"; }
+  // Main test suite
+  void test()
+  {
+    TEST(normal);
+    TEST(valid);
+    TEST(missing);
+  }
+};
+}  // namespace Tests
+
+int main()
+{
+  cout << "\nGrid tester\n===========\n";
+  Tests::tests t;
+  return t.run();
+}
